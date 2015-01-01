1313d05b3ebd4019f201554483183f72dc9fbd5c
santhosh lakkanpalli <santhu221633@gmail.com>
2015-01-01 13:12:16 +0530

    App created.

---
+++ RemotePaint/RemotePaint/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RemotePaint
+//
+//  Created by santhosh lakkanpalli on 01/01/15.
+//  Copyright (c) 2015 com.santhosh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ RemotePaint/RemotePaint/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  RemotePaint
+//
+//  Created by santhosh lakkanpalli on 01/01/15.
+//  Copyright (c) 2015 com.santhosh. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+
+@end
